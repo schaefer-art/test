a5ff75e5bde6bde4b60264ce4c4effe18f9aa0f1
schaefer-art <schaefer-art@mail.de>
2025-09-22 15:38:33 +0200

    1

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Hello Vercel 👋</h1>
+      <p>Dies ist eine Dummy-Seite für den Test-Deploy.</p>
+    </div>
+  );
+}
