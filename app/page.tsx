8c9b2d56d9487017ef76f42b6c59c20223a90261
schaefer-art <schaefer-art@mail.de>
2026-03-09 16:06:11 +0100

    blank

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>Hello Vercel 👋</h1>
-      <p>Dies ist eine minimal funktionierende Next.js-App.</p>
-    </main>
-  );
-}
